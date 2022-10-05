ae7188d82d7b666c2904979ea63186277b585e87
Vaibhav Prajapati <vaibhavprajapati0101@gmail.com>
2022-10-05 12:17:56 +0530

    added nqueen cpp cpde - backtracking

--- reverse-word.py
+++
@@ -1,9 +0,0 @@
-word = str(input("Word : "))
-
-
-def reverse(word):
-  reversed = word[::-1]
-  return reversed
-
-
-print(reverse(word))
--- youtube.py
+++
@@ -1,9 +0,0 @@
-# Author Cool Ravi
-
-import pytube  
-from pytube import YouTube
-import os
-video_url = str(input("Enter youtube video url:"))
-youtube = pytube.YouTube(video_url)  
-video = youtube.streams.first()  
-video.download('C:/Users/+'+os.getlogin()'+/Downloads')  
